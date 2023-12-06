caa9dca8ad19dc58c05cc086c9df309852935aa2
Javier Rizzo <javierrizzoa@gmail.com>
2023-12-06 15:25:59 -0800

    Day 6

---
+++ Day06/part2.py
@@ -0,0 +1,27 @@
+import math
+
+
+def find_roots(time, distance):
+    x1 = (-time + math.sqrt(time**2-4*distance)) / -2
+    x2 = (-time - math.sqrt(time**2-4*distance)) / -2
+    return x1, x2
+
+
+
+with open('input.txt') as f:
+    for line in f:
+        if line.startswith('Time'):
+            times = list(map(int, [line.strip().split(':')[1].replace(' ', '')]))
+        elif line.startswith('Distance'):
+            distances = list(map(int, [line.strip().split(':')[1].replace(' ', '')]))
+
+total = 1
+for i in range(len(times)):
+    r1, r2 = find_roots(times[i], distances[i]+1)
+    r1 = math.ceil(r1)
+    r2 = math.floor(r2)
+    print(str(r1) + " " + str(r2))
+    total *= r2 - r1 + 1
+
+
+print(total)
